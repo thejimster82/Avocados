cda326c6458e3105570212b6b3c9d84da28a7708
thejimster82 <jimmyhwrtn82@gmail.com>
2019-07-26 22:30:21 -0400

    add fit lines to plots

--- avocados.py
+++ avocados.py
@@ -8,7 +8,7 @@ from patsy import dmatrices, dmatrix
 
 # %%
 # help from here https://stackoverflow.com/questions/12604909/pandas-how-to-change-all-the-values-of-a-column
-
+# https://stackoverflow.com/questions/22239691/code-for-best-fit-straight-line-of-a-scatter-plot-in-python
 
 def convertDate(dateStr):
     newDate = datetime.strptime(dateStr, '%Y-%m-%d')
@@ -22,25 +22,44 @@ avocados['Date'] = avocados['Date'].apply(convertDate)
 # %%
 plt.plot_date(avocados.Date, avocados.AveragePrice)
 # %%
-plt.plot(avocados['AveragePrice'][(avocados.region == "West") & (avocados.type == "organic")],
-         avocados['Total Volume'][(avocados.region == "West") & (avocados.type == "organic")], "ro")
-plt.plot(avocados['AveragePrice'][(avocados.region == "West") & (avocados.type == "conventional")],
-         avocados['Total Volume'][(avocados.region == "West") & (avocados.type == "conventional")], "bo")
+x1=avocados['AveragePrice'][(avocados.region == "West") & (avocados.type == "organic")]
+y1=avocados['Total Volume'][(avocados.region == "West") & (avocados.type == "organic")]
+plt.plot(x1,y1, "ro")
+plt.plot(np.unique(x1), np.poly1d(np.polyfit(x1, y1, 1))(np.unique(x1)))
+
+x2=avocados['AveragePrice'][(avocados.region == "West") & (avocados.type == "conventional")]
+y2=avocados['Total Volume'][(avocados.region == "West") & (avocados.type == "conventional")]
+plt.plot(x2,y2, "bo")
+plt.plot(np.unique(x2), np.poly1d(np.polyfit(x2, y2, 1))(np.unique(x2)))
+
 plt.show()
 
 # %%
-plt.plot(avocados['AveragePrice'][(avocados.region == "California") & (avocados.type == "organic")].transform('log'),
-         avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "organic")].transform('log'), "ro")
-plt.plot(avocados['AveragePrice'][(avocados.region == "California") & (avocados.type == "conventional")].transform('log'),
-         avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "conventional")].transform('log'), "bo")
+x1=avocados['AveragePrice'][(avocados.region == "California") & (avocados.type == "organic")].transform('log')
+y1=avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "organic")].transform('log')
+plt.plot(x1, y1, "ro")
+plt.plot(np.unique(x1), np.poly1d(np.polyfit(x1, y1, 1))(np.unique(x1)))
+
+x2=avocados['AveragePrice'][(avocados.region == "California") & (avocados.type == "conventional")].transform('log')
+y2=avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "conventional")].transform('log')
+plt.plot(x2,y2, "bo")
+plt.plot(np.unique(x2), np.poly1d(np.polyfit(x2, y2, 1))(np.unique(x2)))
+
 plt.show()
 
 # %%
 avocados['month'] = pd.DatetimeIndex(avocados['Date']).month
-plt.plot(avocados['month'][(avocados.region == "California") & (avocados.type == "organic")],
-         avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "organic")], "bo")
-plt.plot(avocados['month'][(avocados.region == "California") & (avocados.type == "conventional")],
-         avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "conventional")], "ro")
+x1=avocados['month'][(avocados.region == "California") & (avocados.type == "organic")]
+y1=avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "organic")]
+plt.plot(x1,y1, "bo")
+plt.plot(np.unique(x1), np.poly1d(np.polyfit(x1, y1, 1))(np.unique(x1)))
+
+x2=avocados['month'][(avocados.region == "California") & (avocados.type == "conventional")]
+y2=avocados['Total Volume'][(avocados.region == "California") & (avocados.type == "conventional")]
+plt.plot(x2,y2, "ro")
+plt.plot(np.unique(x2), np.poly1d(np.polyfit(x2, y2, 1))(np.unique(x2)))
+
+plt.show()
 
 # %%
 result, predictors = dmatrices(
